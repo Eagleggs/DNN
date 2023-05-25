545450a34bdf1a53c92569b1bf9e5ddf74b4cfeb
Junyu Lu <J.Lu-17@student.tudelft.nl>
2023-05-25 15:09:17 +0200

    Change inference

--- inference.py
+++ inference.py
@@ -5,6 +5,8 @@ from torch import optim
 from tqdm import tqdm
 import torch
 import torch.nn as nn
+
+from get_data import highpassfilter
 from transformerLite import TransformerLite
 
 
@@ -25,19 +27,30 @@ def infer(MAX_LENGTH=1000):
     args = parser.parse_args()
 
     # Load the model (specify the map_location parameter to load on CPU)
-    model = torch.load('model.pt', map_location=torch.device('cpu'))
+    model = torch.load('model_best.pt', map_location=torch.device('cpu'))
 
     device = torch.device('cpu')
     model = model.to(device)
 
     # Load and process the PCM file
-    pcmfile = load_file(args.pcmName)
-    if pcmfile.size(1) > MAX_LENGTH:
-        pcmfile = pcmfile[:, 10000:10000 + MAX_LENGTH, :]
-    pcmfile = pcmfile.to(device)
+    with open(args.pcmName, 'rb') as f:
+        pcm_data = np.frombuffer(f.read(), dtype=np.int16)
+    hi = highpassfilter();
+    pcm_data = hi.butter_highpass_filter(pcm_data, 18000, 63333)
+    abs_array = np.abs(pcm_data)
+    index = np.argmax(abs_array > 1000) + 320
+    waveform = torch.from_numpy(pcm_data.copy()[index:index + 2000]).float()  # 5ms = 320 samples
+    waveform = waveform.unsqueeze(1)
+    time_index = torch.arange(waveform.shape[0]).unsqueeze(1)
+    waveform = torch.cat((waveform, time_index), dim=1)
+    waveform = torch.unsqueeze(waveform,0)
+    print(waveform.shape)
+    # if pcmfile.size(1) > MAX_LENGTH:
+    #     pcmfile = pcmfile[:, 10000:10000 + MAX_LENGTH, :]
+    waveform = waveform.to(device)
 
     # Perform the inference
-    output = model(pcmfile)
-    print(f"The prediction of this room is: Room {(output)}")
+    output = model(waveform)
+    print(f"The prediction of this room is: Room {(torch.argmax(output, dim=1))}")
 
 infer()
