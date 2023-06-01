2b206070b0eae17df0a7a7a7a01471bb4a033936
Junyu Lu <J.Lu-17@student.tudelft.nl>
2023-06-01 17:16:21 +0200

    change infer

--- inference.py
+++ inference.py
@@ -3,6 +3,7 @@ import argparse
 import numpy as np
 import torch
 import torch.nn as nn
+from scipy import signal
 
 from get_data import highpassfilter
 from transformerLite import TransformerLite
@@ -23,9 +24,9 @@ def infer():
     parser = argparse.ArgumentParser(prog='Path', epilog='Text at the bottom of help')
     parser.add_argument('pcmName')
     args = parser.parse_args()
-    model = TransformerLite(t=1500, k=2, heads=8)
+    model = TransformerLite(t=333, k=501, heads=16)
     # Load the model (specify the map_location parameter to load on CPU)
-    model.load_state_dict(torch.load('model_best_30_test.pt', map_location=torch.device('cpu')))
+    model.load_state_dict(torch.load('model_best_1.pt', map_location=torch.device('cpu')))
 
     device = torch.device('cpu')
     model = model.to(device)
@@ -34,34 +35,42 @@ def infer():
     with open(args.pcmName, 'rb') as f:
         pcm_data = np.frombuffer(f.read(), dtype=np.int16)
     hi = highpassfilter();
-    pcm_data = hi.butter_highpass_filter(pcm_data, 14000, 63333)
-    abs_array = np.abs(pcm_data)
+    pcm_data = hi.butter_highpass_filter(pcm_data, 2000, 63333)
 
     index_f = 0
-    amplitude = 3000
+    amplitude = 10000
     while index_f > 16000 or index_f < 8000:
         index_f = np.argmax(pcm_data > amplitude)
         amplitude += 100
-        if amplitude > 4000:
+        if amplitude > 30000:
             break
     max = np.max(pcm_data[index_f:index_f + 3000])
     if np.max(pcm_data) != max:
         print("1")
-    indices = np.where(pcm_data > max / 5)[0]
+    max = np.max(pcm_data[index_f:index_f + 3000])
+    if np.max(pcm_data) != max:
+        print(np.max(pcm_data))
+    indices = np.where(pcm_data > max - 5000)[0]
     indices = indices[indices < index_f + 3000]
     id = np.max(indices)
-    waveform = torch.from_numpy(pcm_data.copy()[id:id + 1500]).float()
-    waveform = waveform.unsqueeze(1)
-    time_index = torch.arange(waveform.shape[0]).unsqueeze(1)
-    waveform = torch.cat((waveform, time_index), dim=1)
-    waveform = torch.unsqueeze(waveform,0)
+    waveform = torch.from_numpy(pcm_data.copy()[id:id + 3000]).float()
+    freq, t, stft = signal.spectrogram(waveform, fs=63333, mode='magnitude', nperseg=10, noverlap=1, nfft=1000)
+    stft = torch.from_numpy(stft.T.copy()).float()
+    # indices = np.where(pcm_data > max / 5)[0]
+    # indices = indices[indices < index_f + 3000]
+    # id = np.max(indices)
+    # waveform = torch.from_numpy(pcm_data.copy()[id:id + 1500]).float()
+    # waveform = waveform.unsqueeze(1)
+    # time_index = torch.arange(waveform.shape[0]).unsqueeze(1)
+    # waveform = torch.cat((waveform, time_index), dim=1)
+    # waveform = torch.unsqueeze(waveform,0)
     # if pcmfile.size(1) > MAX_LENGTH:
     #     pcmfile = pcmfile[:, 10000:10000 + MAX_LENGTH, :]
-    waveform = waveform.to(device)
+    # waveform = waveform.to(device)
 
     # Perform the inference
-    output = model(waveform)
-    print(f"The prediction of this room is: Room {(torch.argmax(output, dim=1) + 1)}")
+    output = model(torch.unsqueeze(stft,dim=0))
+    print(f"The prediction of this room is: Room {(torch.argmax(output, dim=1) +5)}")
     print(output)
 
 infer()
